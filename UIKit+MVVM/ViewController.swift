d1c038b140c79eef24e3d5be9d55227a457d1a6a
moonjs0113 <littlemoom@naver.com>
2022-07-16 13:25:50 +0900

    Create Project

---
+++ UIKit+MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit+MVVM
+//
+//  Created by Moon Jongseek on 2022/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
