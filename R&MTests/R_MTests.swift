a7204eb49decf75dcbee779f0c6aea1b43e28720
moonjs0113 <littlemoom@naver.com>
2023-12-16 20:22:14 +0900

    CI/CD Test 6

--- R&MTests/R_MTests.swift
+++ R&MTests/R_MTests.swift
@@ -32,5 +32,5 @@ final class R_MTests: XCTestCase {
 //    func testPerformanceExample() throws {
 //        // This is an example of a performance test case.
 //        measure {
-//            // Put the code you want to measure the time of here.
+//            //
 }
